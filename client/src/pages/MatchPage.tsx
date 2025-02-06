5bedb6df18639b9494d8ecc99d6b8941087a42d4
Julien PICART <julienpicart01@gmail.com>
2025-02-06 17:42:45 +0100

    Matchpage : in progress

--- client/src/pages/MatchPage.tsx
+++ client/src/pages/MatchPage.tsx
@@ -12,7 +12,7 @@ export default function MatchPage() {
         backgroundImage: `linear-gradient(rgba(11, 11, 11, 0.7), rgba(11, 11, 11, 0.7)), url('/public/matchpage_background.png')`,
       }}
     >
-      <Link to={"/firstpage"} className="close-match">
+      <Link to={"/home"} className="close-match">
         X
       </Link>
       <div className="match">
